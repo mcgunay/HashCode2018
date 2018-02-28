01d253245fdb4564da7138bee1247e67326f1821
can <neo.can@gmail.com>
2018-02-28 14:51:06 +0300

    comment eklendi

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello Git");
             Console.ReadKey();
+            /*bu comment oldu mu buraya?*/
         }
     }
 }
